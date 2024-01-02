71ce51bf3256b99d4494feae3cd735ef6eb3319b
suhyunChun <suhyun.chun@stonybrook.edu>
2024-01-02 19:59:20 +0900

    [Add] Load, InstantiatePrefab, Destroy function

--- MiniRPG/Assets/Scripts/Managers/ResourceManager.cs
+++ MiniRPG/Assets/Scripts/Managers/ResourceManager.cs
@@ -1,7 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
 namespace Managers
 {
     public class ResourceManager
     {
-        
+        public Dictionary<string, Object> resources;
+
+        public ResourceManager()
+        {
+            resources = new Dictionary<string, Object>();
+        }
+        public T Load<T>(string key) where T : Object
+        {
+            return Resources.Load<T>(key);
+        }
+
+        public GameObject InstantiatePrefab(string path, Transform transform = null)
+        {
+            GameObject resource = Load<GameObject>($"Prefabs/{path}");
+            if (resource == null)
+            {
+                Debug.LogError($"Failed to load Prefab: {path}");
+                return null;
+            }
+            return Object.Instantiate(resource, transform);
+        }
+
+
+        public void Destroy(GameObject obj)
+        {
+            if (obj == null) return;
+            Object.Destroy(obj);
+        }
+
     }
 }
