f4970f068f7846f7e751ca5842474713ca4caa8f
I'm Developer? <59020441+iamdeveloperz@users.noreply.github.com>
2024-01-02 22:01:06 +0900

    [➕ ADD] ExpPoint, ManaPoint
    # EXP, MANA 모듈 추가

--- MiniRPG/Assets/Scripts/Models/Player/Modules/ExperiencePoint.cs
+++ MiniRPG/Assets/Scripts/Models/Player/Modules/ExperiencePoint.cs
@@ -1,8 +1,7 @@
 
 using UnityEngine;
 
-[System.Serializable]
-public class HealthPoint : IStatus
+public class ExperiencePoint : IStatus
 {
     #region Properties
 
@@ -15,13 +14,13 @@ public class HealthPoint : IStatus
 
     #region Constructor
 
-    public HealthPoint(float setValue)
+    public ExperiencePoint(float setValue)
     {
         MaxValue = setValue;
         CurrentValue = MaxValue;
     }
 
-    public HealthPoint(float curValue, float maxValue)
+    public ExperiencePoint(float curValue, float maxValue)
     {
         CurrentValue = curValue;
         MaxValue = maxValue;
@@ -35,19 +34,16 @@ public class HealthPoint : IStatus
 
     public void AddValue(float amount)
     {
-        // 현재 값에 amount를 추가 하되, 최대 값을 초과하지 않는다.
         CurrentValue = Mathf.Min(CurrentValue + amount, MaxValue);
     }
 
     public void SubValue(float amount)
     {
-        // 현재 값에 amount를 차감 하되, 0보다 작아지지 않는다.
         CurrentValue = Mathf.Max(CurrentValue - amount, 0f);
     }
 
     public float GetPercentage()
     {
-        // 현재 값과 최대 값의 비율 계산
         return MaxValue > 0 ? CurrentValue / MaxValue : 0;
     }
 
--- MiniRPG/Assets/Scripts/Models/Player/Modules/ManaPoint.cs
+++ MiniRPG/Assets/Scripts/Models/Player/Modules/ManaPoint.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 
 [System.Serializable]
-public class HealthPoint : IStatus
+public class ManaPoint : IStatus
 {
     #region Properties
 
@@ -15,13 +15,13 @@ public class HealthPoint : IStatus
 
     #region Constructor
 
-    public HealthPoint(float setValue)
+    public ManaPoint(float setValue)
     {
         MaxValue = setValue;
         CurrentValue = MaxValue;
     }
 
-    public HealthPoint(float curValue, float maxValue)
+    public ManaPoint(float curValue, float maxValue)
     {
         CurrentValue = curValue;
         MaxValue = maxValue;
@@ -35,19 +35,16 @@ public class HealthPoint : IStatus
 
     public void AddValue(float amount)
     {
-        // 현재 값에 amount를 추가 하되, 최대 값을 초과하지 않는다.
         CurrentValue = Mathf.Min(CurrentValue + amount, MaxValue);
     }
 
     public void SubValue(float amount)
     {
-        // 현재 값에 amount를 차감 하되, 0보다 작아지지 않는다.
         CurrentValue = Mathf.Max(CurrentValue - amount, 0f);
     }
 
     public float GetPercentage()
     {
-        // 현재 값과 최대 값의 비율 계산
         return MaxValue > 0 ? CurrentValue / MaxValue : 0;
     }
 
