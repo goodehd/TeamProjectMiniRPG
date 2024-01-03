fd858167b977965cf60100ad3b2d44344d17c35b
suhyunChun <suhyun.chun@stonybrook.edu>
2024-01-03 19:26:53 +0900

    [Add] ResourceManager - complete function implementation

--- MiniRPG/Assets/Scripts/Managers/ResourceManager.cs
+++ MiniRPG/Assets/Scripts/Managers/ResourceManager.cs
@@ -1,25 +1,131 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceLocations;
+using Object = UnityEngine.Object;
 
 namespace Managers
 {
     public class ResourceManager
     {
-        public Dictionary<string, Object> resources;
+        public Dictionary<string, Object> _resources;
 
-        public ResourceManager()
+        public ResourceManager(){
+            _resources = new Dictionary<string,Object>();
+    }
+
+
+        public void AllLoadResource<T>(string label, Action<string,int,int> callback) where T : Object
         {
-            resources = new Dictionary<string, Object>();
+            AsyncOperationHandle<IList<IResourceLocation>> operation = Addressables.LoadResourceLocationsAsync(label, typeof(T));
+            operation.Completed += operationHandle =>
+            {
+                int loadCount = 0;
+                int totalCount = operationHandle.Result.Count;
+                foreach (var res in operationHandle.Result)
+                {
+                    LoadResource<T>(res.PrimaryKey, obj =>
+                    {
+                        loadCount++;
+                        callback?.Invoke(res.PrimaryKey, loadCount, totalCount);
+                    });
+                }
+            };
         }
-        public T Load<T>(string key) where T : Object
+    
+        private void AtlasCallbackFunction<T>(string key, AsyncOperationHandle<IList<T>> handle, Action<IList<T>> callback) where T : Object
         {
-            return Resources.Load<T>(key);
+            handle.Completed += operationHandle =>
+            {
+                foreach (var result in operationHandle.Result)
+                {
+                    string keyIndex = result.ToString().Split("_")[1].Split(" ")[0];
+                    string resourceKey = $"{key}[{keyIndex}]";
+
+                    if (!_resources.ContainsKey(resourceKey))
+                    {
+                        _resources.Add(resourceKey, result);
+                    }
+                }
+                callback?.Invoke(operationHandle.Result);
+            };
+        }
+
+        private void HandlerCallbackFunction<T>(string key, AsyncOperationHandle<T> handle, Action<T> callback) where T :Object
+        {
+            handle.Completed += operationHandle =>
+            {
+                if (!_resources.ContainsKey(key))
+                {
+                    _resources.Add(key, operationHandle.Result);
+                }
+               
+                callback?.Invoke(operationHandle.Result);
+            };
+        }
+
+        private void TextCallbackFunction<TextAsset>(string key, AsyncOperationHandle<TextAsset> handle, Action<TextAsset> callback) where TextAsset : Object
+        {
+                handle.Completed += operationHandle => {
+                //    var textList= operationHandle.Result;
+                 //   foreach (var item in textList)
+                   // {
+                        if (!_resources.ContainsKey(key))
+                            _resources.Add(key, operationHandle.Result);
+                   // }
+
+                callback?.Invoke(operationHandle.Result);
+            };
+        }
+
+        private void LoadResource<T>(string key, Action<T> callback =null) where T: Object
+        {
+            string loadKey = key;
+            if(_resources.TryGetValue(loadKey, out Object resource))
+            {
+                callback?.Invoke(resource as T);
+                return;
+            }
+
+            if (key.Contains(".atlas")) //atlas 
+            {
+                AsyncOperationHandle<IList<Sprite>> handle = Addressables.LoadAssetAsync<IList<Sprite>>(loadKey);
+                AtlasCallbackFunction(key, handle, objs => callback?.Invoke(objs as T));
+            }
+            else if (key.Contains(".sprite")) //sprite
+            {
+                loadKey = $"{key}[{key.Replace(".sprite", "")}]";
+                AsyncOperationHandle<Sprite> handle = Addressables.LoadAssetAsync<Sprite>(loadKey);
+                HandlerCallbackFunction(loadKey, handle, callback as Action<Sprite>);
+
+            }
+            /*
+            else if (key.Contains(".text")) //text (csv) 
+            { 
+                
+                loadKey = $"{key.Replace(".text", "")}";
+                AsyncOperationHandle<TextAsset> handle = Addressables.LoadAssetAsync<TextAsset>(loadKey);
+                TextCallbackFunction(loadKey, handle, callback as Action<TextAsset>);
+            }
+            */
+            else // object
+            {
+                AsyncOperationHandle<T> handle = Addressables.LoadAssetAsync<T>(loadKey);
+                HandlerCallbackFunction(loadKey, handle, callback);
+            }
+        }
+        public T Load<T>(string path) where T : Object
+        {
+            if (_resources.TryGetValue(path, out Object resource)) return resource as T;
+            Debug.LogError($"Failed to find key : {path}");
+            return null;
         }
 
         public GameObject InstantiatePrefab(string path, Transform transform = null)
         {
-            GameObject resource = Load<GameObject>($"Prefabs/{path}");
+            GameObject resource = Load<GameObject>($"{path}.prefab");  
             if (resource == null)
             {
                 Debug.LogError($"Failed to load Prefab: {path}");
@@ -29,11 +135,21 @@ namespace Managers
         }
 
 
-        public void Destroy(GameObject obj)
+        public void UnloadAllAsync<T>(string label) where T : Object
         {
-            if (obj == null) return;
-            Object.Destroy(obj);
+            var operation = Addressables.LoadResourceLocationsAsync(label, typeof(T));
+            operation.Completed += operationHandle =>
+            {
+                foreach (var result in operationHandle.Result)
+                {
+                    if (_resources.TryGetValue(result.PrimaryKey, out Object resource))
+                    {
+                        Addressables.Release(resource);
+                        _resources.Remove(result.PrimaryKey);
+                    }
+     
+                }
+            };
         }
-
     }
 }
