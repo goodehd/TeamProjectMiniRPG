1ab91151071d69161e6d8af3a699a5680a24719e
Song_ANT <songhagyeom@gmail.com>
2024-01-04 19:11:02 +0900

    [ADD] UI_BaseScene_AddEventSystem
    프리팹화된 EventSystem도 BaseScene에서 불러오기

--- MiniRPG/Assets/Scripts/Scene/IntroScene.cs
+++ MiniRPG/Assets/Scripts/Scene/IntroScene.cs
@@ -8,13 +8,16 @@ namespace Scene
         protected override bool Initialized()
         {
             if (!base.Initialized()) return false;
-            Main.Resource.AllLoadResource<Object>("Preload", (key, loadCount, totalCount) => { 
-                if(loadCount == totalCount) 
+            Main.Resource.AllLoadResource<Object>("Preload", (key, loadCount, totalCount) =>
+            {
+                if (loadCount == totalCount)
                 {
                     UI.SetSceneUI<TestUI>();
+
                 }
             });
-            
+            //UI.SetSceneUI<TestUI>();
+
             return true;
         }
     }
