23306bfd6419fecd9dfa8d0ee6893671e2dc2055
suhyunChun <suhyun.chun@stonybrook.edu>
2024-01-05 17:05:57 +0900

    [Add] ItemManager setting

--- MiniRPG/Assets/Scripts/Managers/ItemManager.cs
+++ MiniRPG/Assets/Scripts/Managers/ItemManager.cs
@@ -2,6 +2,14 @@ namespace Managers
 {
     public class ItemManager
     {
-        
+        public void Init()
+        {
+            SetItemData();
+
+        }
+        private void SetItemData() //한줄에 다섯개씩 
+        {
+
+        }
     }
 }
