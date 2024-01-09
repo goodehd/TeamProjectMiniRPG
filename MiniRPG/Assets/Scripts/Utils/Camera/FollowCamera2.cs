3c398e93720b67cae0a872c1ba4be99b529954af
oteosuk <oteosuk@naver.com>
2024-01-09 10:53:45 +0900

    [Update] - 카메라 회전 변경

--- MiniRPG/Assets/Scripts/Utils/Camera/FollowCamera2.cs
+++ MiniRPG/Assets/Scripts/Utils/Camera/FollowCamera2.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class FollowCamera2 : MonoBehaviour
 {
     public Transform target { get; set; }
-    private Vector3 _disToPlayer = new Vector3(8f, 16f, -11f);
+    private Vector3 _disToPlayer = new Vector3(13f, 16f, 0f);
+    private Quaternion _desiredRotation = Quaternion.Euler(50f, -100f, 0f);
 
     void LateUpdate()
     {
@@ -15,5 +16,7 @@ public class FollowCamera2 : MonoBehaviour
         }
         transform.position = target.position + _disToPlayer;
         transform.LookAt(target);
+
+        transform.rotation = _desiredRotation;
     }
 }
